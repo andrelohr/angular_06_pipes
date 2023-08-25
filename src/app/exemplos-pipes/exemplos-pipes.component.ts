6b1076a2620e50b4289b4529767058041738fc8e
Andre Lohr <andrefelipelohr@gmail.com>
2023-08-25 12:17:52 -0300

    Pipes: pipes, parâmetros e aninhamento

---
+++ src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplos-pipes',
+  templateUrl: './exemplos-pipes.component.html',
+  styleUrls: ['./exemplos-pipes.component.css']
+})
+export class ExemplosPipesComponent {
+
+  livro: any = {
+    titulo: 'Effective Java 3rd Edition',
+    rating: 4.65432,
+    numeroPaginas: 417,
+    preco: 178.03,
+    dataLancamento: new Date(2017, 11, 27),
+    url: 'https://www.amazon.com/Effective-Java-Joshua-Bloch/dp/0134685997/'
+  }
+
+}
