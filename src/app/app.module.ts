5f0639f181b4fcc52387d4a169581464056a98a6
Andre Lohr <andrefelipelohr@gmail.com>
2023-08-26 00:52:37 -0300

    Aplicando locale aos pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service';
+import { registerLocaleData } from '@angular/common';
+import localePT from '@angular/common/locales/pt';
+registerLocaleData(localePT);
 
 @NgModule({
   declarations: [
@@ -14,7 +18,15 @@ import { CamelCasePipe } from './camel-case.pipe';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    //SettingsService,
+    {
+      provide: LOCALE_ID,
+      //deps: [SettingsService]
+      //useClass: SettingsService
+      useValue: 'pt-BR'
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
